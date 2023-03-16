f8a8d065fab55677e5787ac4478eaae212e49e63
RaulGabG <raulgguanca@gmail.com>
2023-03-16 10:00:19 -0300

    added logic to stats.js

--- js/stats.js
+++ js/stats.js
@@ -6,9 +6,38 @@ setTimeout(() => {
 
   const pastEventTab = document.getElementById("pastEventsTab");
 
-  //<tr>
-  //<td></td>
-  //<td></td>
-  //<td></td>
-  //</tr>;
+  const arrayEvents = allEvents.events;
+
+  const getCurrentDate = allEvents.currenDate;
+
+  //functions
+  let pastEvents = [];
+
+  for (const event of arrayEvents) {
+    if (getCurrentDate > event.date) {
+      pastEvents.push(event);
+    }
+  }
+
+  function statsRanking(events) {
+    let evnStas = "";
+    if (!evnStas.length > 3) {
+      for (const evnt of events.sort()) {
+        evnStas += `
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        `;
+      }
+    }
+    return evnStas;
+  }
+
+  let evntCreator = statsRanking(arrayEvents);
+
+  function paintEvents() {
+    eventStdtcsTab.innerHTML = evntCreator;
+  }
 }, 1500);
