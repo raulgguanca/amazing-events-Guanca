b542c1ef71dd9c75622693c29d1a4400f1f0d9f4
RaulGabG <raulgguanca@gmail.com>
2023-03-17 20:53:03 -0300

    added filter and content for event stadistics in stats page

--- js/stats.js
+++ js/stats.js
@@ -2,15 +2,26 @@ setTimeout(() => {
   //variables
   const eventStdtcsTab = document.getElementById("eventStadisticsTab");
 
+  const top1 = document.getElementById("top1");
+  const top2 = document.getElementById("top2");
+  const top3 = document.getElementById("top3");
+
+  const bttn1 = document.getElementById("bttn1");
+  const bttn2 = document.getElementById("bttn2");
+  const bttn3 = document.getElementById("bttn3");
+
+  const lrg1 = document.getElementById("lrg1");
+  const lrg2 = document.getElementById("lrg2");
+  const lrg3 = document.getElementById("lrg3");
+
   const upcEventTab = document.getElementById("upcEventsTab");
 
   const pastEventTab = document.getElementById("pastEventsTab");
 
   const arrayEvents = allEvents.events;
 
-  const getCurrentDate = allEvents.currenDate;
+  const getCurrentDate = allEvents.currentDate;
 
-  //functions
   let pastEvents = [];
 
   for (const event of arrayEvents) {
@@ -19,25 +30,82 @@ setTimeout(() => {
     }
   }
 
-  function statsRanking(events) {
-    let evnStas = "";
-    if (!evnStas.length > 3) {
-      for (const evnt of events.sort()) {
-        evnStas += `
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        `;
-      }
+  //functions
+  //
+  //filter for events
+  function filterEventsPerformance(arrayEv) {
+    let performance = [];
+
+    arrayEv.forEach((element) => {
+      let perform = {};
+      perform.id = element._id;
+      perform.name = element.name;
+      perform.assistance = element.assistance;
+      perform.capacity = element.capacity;
+      perform.category = element.category;
+      perform.percentage = (100 * element.assistance) / element.capacity;
+      perform.price = element.price;
+      performance.push(perform);
+    });
+    return performance;
+  }
+
+  //sort of top events
+  const allEventsPerformance = filterEventsPerformance(pastEvents).sort(
+    (perf1, perf2) => {
+      if (perf1.percentage < perf2.percentage) return 1;
+      if (perf1.percentage > perf2.percentage) return -1;
+      return 0;
     }
-    return evnStas;
-  }
+  );
 
-  let evntCreator = statsRanking(arrayEvents);
+  console.log(allEventsPerformance);
+  //create table content
+  //
+  //highest percetage
+  top1.innerText = `${allEventsPerformance[0].name} ${Number.parseFloat(
+    allEventsPerformance[0].percentage
+  ).toFixed(2)}%`;
+  top2.innerText = `${allEventsPerformance[1].name} ${Number.parseFloat(
+    allEventsPerformance[1].percentage
+  ).toFixed(2)}%`;
+  top3.innerText = `${allEventsPerformance[2].name} ${Number.parseFloat(
+    allEventsPerformance[2].percentage
+  ).toFixed(2)}%`;
 
-  function paintEvents() {
-    eventStdtcsTab.innerHTML = evntCreator;
-  }
+  //botton percetage
+  const bottonPerformace = allEventsPerformance.slice().reverse();
+
+  bttn1.innerText = `${bottonPerformace[0].name} ${Number.parseFloat(
+    bottonPerformace[0].percentage
+  ).toFixed(2)}%`;
+  bttn2.innerText = `${bottonPerformace[1].name} ${Number.parseFloat(
+    bottonPerformace[1].percentage
+  ).toFixed(2)}%`;
+  bttn3.innerText = `${bottonPerformace[2].name} ${Number.parseFloat(
+    bottonPerformace[2].percentage
+  ).toFixed(2)}%`;
+
+  //larger capacity
+  const capacityPerformance = allEventsPerformance
+    .slice()
+    .sort((perf1, perf2) => {
+      if (perf1.capacity < perf2.capacity) return 1;
+      if (perf1.capacity > perf2.capacity) return -1;
+      return 0;
+    });
+
+  lrg1.innerText = `${capacityPerformance[0].name} ${Number.parseInt(
+    capacityPerformance[0].capacity
+  )}`;
+  lrg2.innerText = `${capacityPerformance[1].name} ${Number.parseInt(
+    capacityPerformance[1].capacity
+  )}`;
+  lrg3.innerText = `${capacityPerformance[2].name} ${Number.parseInt(
+    capacityPerformance[2].capacity
+  )}`;
+
+  //sort of upcoming events
+
+  //sort of past events
 }, 1500);
