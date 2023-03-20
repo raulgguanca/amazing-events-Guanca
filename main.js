c855e3ae96aab1a2e1f8139183f985915e9102b2
RaulGabG <raulgguanca@gmail.com>
2023-03-20 17:25:16 -0300

    changed to async function in main page

--- main.js
+++ main.js
@@ -5,26 +5,18 @@ const urlApiAE = "https://mindhub-xj03.onrender.com/api/amazing";
 let allEvents = [];
 let categories = [];
 
-//async function getEvents() {
-//try {
-//const response = await fetch(urlApiAE);
-//let data = await response.json();
-//allEvents = data;
-//return data;
-//} catch (e) {
-//console.log(e);
-//}
-//}
-
-fetch(urlApiAE)
-  .then((Response) => Response.json())
-  .then((data) => {
+async function getEvents() {
+  try {
+    let response = await fetch(urlApiAE);
+    let data = await response.json();
     allEvents = data;
 
     for (const cat of allEvents.events) {
       if (!categories.includes(cat.category)) categories.push(cat.category);
     }
-  })
-  .catch((e) => {
+  } catch (e) {
     console.log(e);
-  });
+  }
+}
+
+getEvents();
